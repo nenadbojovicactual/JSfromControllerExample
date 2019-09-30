ec15d1b20529e680b03e7a6d0d91ee380c367ee1
nenadbojovicactual <bojovictheman@gmail.com>
2019-09-30 06:31:38 +0200

    Add project files.

---
+++ JSfromControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSfromControllerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult TestResWrite()
+        {
+            // vraca js ili html iz kontrollera
+            Response.Write("<script>$('#reswrite').html('js iz kontrolera')</script>");
+            // vraca partial view
+            return PartialView();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ JSfromControllerExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSfromControllerExample.MvcApplication" Language="C#" %>
